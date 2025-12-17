de8614579be406be92e920fee4cee4b3bc8d9274
Vishnu Kumar Ghosh <vishnu.klhs@gmail.com>
2025-12-17 13:23:43 +0530

    FUnction Decorator,Module Packages done

---
+++ import_module.py
@@ -0,0 +1,9 @@
+import datetime
+x=datetime.datetime.now()
+print(x.strftime("%c"))
+
+# a=pow(4,3)
+# print(a)
+
+a=abs(-5.25)
+print(a)
---
+++ mymodule.py
@@ -0,0 +1,14 @@
+# def greeting(name):
+#     print("Hello",name)
+# # greeting("Rahul")
+details={
+    "Name":"Sawan",
+    "Age":25,
+    "Country":'India',
+    "Marital_Status":"Unmarried"
+}
+account={
+    "Account_Number":15842544,
+    "Bank_Name":"SBI Bank"
+    
+}
